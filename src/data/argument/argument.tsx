04508776ad82b9132b7ecb124acb6a1a1b64da13
aslogd <gerarddelcastillo@gmail.com>
2020-04-25 13:33:35 +0200

    imporve argument definition

--- src/data/argument/argument.tsx
+++ src/data/argument/argument.tsx
@@ -5,26 +5,36 @@ export interface Argument {
 }
 
 export interface Node {
+  type: NodeType
   id: number
   sentence: string
 }
 
+export enum NodeType {
+  STATEMENT,
+  FACT,
+  SOURCE
+}
+
 export interface Statement extends Node {
-  children?: Node[]
+  type: NodeType.STATEMENT
+  children?: (Statement | Fact)[]
 }
 
 export interface Fact extends Node {
+  type: NodeType.FACT
   sources: Source[]
 }
 
-export interface Source{
-  name: string,
-  href: string,
+export interface Source extends Node{
+  type: NodeType.SOURCE
+  href: string
   description: string
 }
 
 export function createEmptyArgument(id: number) : Argument {
   return {
     id: id,
+    root: 
   }
 }
