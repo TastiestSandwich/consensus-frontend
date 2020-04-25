347a00cc5369f77c584e6ec234933e266a367f3e
sergi <sergicruzdes@gmail.com>
2020-04-25 18:00:49 +0200

    Node editor in progress

---
+++ src/components/nodeEditor.tsx
@@ -0,0 +1,158 @@
+import React from "react"
+import { Node, NodeType, Statement, Fact, Source } from "../data/argument/argument"
+
+interface NodeEditorProps {
+  node: Node
+  save: () => void
+}
+
+interface NodeEditorState {
+  sentence: string
+  childrenCount: number
+  type: NodeType
+  href?: string
+  description?: string
+  step: EditorStep
+}
+
+enum EditorStep {
+  SENTENCE,
+  HREF,
+  DESCRIPTION,
+  TYPE,
+  ADDNODE
+}
+
+export default class NodeEditor extends React.Component<NodeEditorProps, NodeEditorState> {
+  constructor(props) {
+    super(props)
+
+    switch(props.node.type) {
+      case NodeType.FACT: {
+        let node : Fact = props.node as Fact
+        this.state = {
+          sentence: node.sentence,
+          childrenCount: node.sources.length,
+          type: NodeType.FACT,
+          step: EditorStep.SENTENCE
+        }
+      }
+      case NodeType.SOURCE: {
+        let node : Source = props.node as Source
+        this.state = {
+          sentence: node.sentence,
+          childrenCount: 0,
+          href: node.href,
+          description: node.description,
+          type: NodeType.SOURCE,
+          step: EditorStep.SENTENCE
+        }
+      }
+      // Default is NodeType.STATEMENT
+      default: {
+        let node : Statement = props.node as Statement
+        this.state = {
+          sentence: node.sentence,
+          childrenCount: node.children ? node.children.length : 0,
+          type: NodeType.STATEMENT,
+          step: EditorStep.SENTENCE
+        }
+      }
+    }
+  }
+
+  updateSentence = (sentence: string) => {
+    this.setState({
+      sentence: sentence
+    })
+  }
+
+  handleSaveSentence = (sentence: string) => {
+    this.setState({
+      sentence: sentence
+    })
+  }
+
+  renderNodePreview() {
+    const sentence = this.state.sentence
+    return(
+      <div className="node-editor__sentence">
+         { sentence }
+      </div>
+    )
+  }
+
+  renderSentenceStep() {
+    return(
+      <div className="node-editor__actions">
+        <div className="node-editor__message">
+          Add a sentence
+        </div>
+      </div>
+    )
+  }
+
+  renderTypeStep() {
+    return(
+      <div className="node-editor__actions">
+        <div className="node-editor__message">
+          Is this a Statement, a Fact or a Source?
+        </div>
+        <div className="node-editor__type-buttons">
+          <button className="node-editor__statement-button">STATEMENT</button>
+          <button className="node-editor__fact-button">FACT</button>
+          <button className="node-editor__source-button">SOURCE</button>
+        </div>
+      </div>
+    )
+  }
+
+  renderAddNodeStep() {
+     return(
+      <div className="node-editor__actions">
+        <div className="node-editor__message">
+          Add a Child or a Sibling
+        </div>
+        <div className="node-editor__add-node-buttons">
+          <button className="node-editor__child-button">CHILD</button>
+          <button className="node-editor__sibling-button">SIBLING</button>
+        </div>
+      </div>
+    )
+  }
+
+  renderNextStepButton() {
+    return(
+      <div className="node-editor__next-step">
+        <button className="node-editor__next-step-button" onClick={this.handleNextStep}>
+      </div>
+    )
+  }
+
+  renderEditorActions() {
+    switch(this.state.step) {
+      case EditorStep.SENTENCE: {
+        return this.renderSentenceStep
+      }
+      case EditorStep.TYPE: {
+        return this.renderTypeStep
+      }
+      case EditorStep.ADDNODE: {
+        return this.renderAddNodeStep
+      }
+      default: {
+        return this.renderSentenceStep
+      }
+    }
+  }
+
+  render() {
+    return(
+      <div className="node-editor">
+        { this.renderNodePreview }
+        { this.renderEditorActions }
+      </div>
+    )
+  }
+
+}
--- src/views/creator/creator.tsx
+++ src/views/creator/creator.tsx
@@ -2,22 +2,24 @@ import React from "react"
 import * as api from "../../api"
 import {
 	Argument,
-	createEmptyArgument
+	createEmptyArgument,
+	Node
 } from "../../data/argument/argument"
 import ArgumentRender from "../../components/argumentRender"
+import NodeEditor from "../../components/nodeEditor"
 
 interface CreatorProps { }
 
 interface CreatorState {
 	loading: boolean
-	selectedNodeId: number
+	selectedNode: Node | null
 	argument?: Argument
 }
 
 export default class Creator extends React.Component<CreatorProps, CreatorState> {
 	state: CreatorState = {
 		loading: true,
-		selectedNodeId: -1
+		selectedNode: null
 	}
 
 	componentDidMount() {
@@ -25,9 +27,19 @@ export default class Creator extends React.Component<CreatorProps, CreatorState>
 			const argument = createEmptyArgument(id)
 			this.setState({
 				loading: false,
-				argument,
-				selectedNodeId: argument.root.id
+				argument: argument,
+				selectedNode: argument.root
 			})
+		})
+	}
+
+	handleSaveSelectedNode = (node: Node) => {
+		//TODO search node in argument and substitute
+		let argument = {...this.state.argument} as Argument
+		argument.root = node
+		this.setState({
+			selectedNode: node,
+			argument: argument
 		})
 	}
 
@@ -37,12 +49,18 @@ export default class Creator extends React.Component<CreatorProps, CreatorState>
 		}
 
 		let argument = this.state.argument as Argument
+		let selectedNode = this.state.selectedNode as Node
 
 		return (
 			<div className="creator-component">
 				<ArgumentRender 
 					argument={argument} 
-					selectedNodeId={this.state.selectedNodeId} 
+					selectedNodeId={selectedNode.id} 
+				/>
+				<NodeEditor
+					//TODO search node from selectedNodeId
+					node={selectedNode}
+					save={() => this.handleSaveSelectedNode}
 				/>
 			</div>
 		)
